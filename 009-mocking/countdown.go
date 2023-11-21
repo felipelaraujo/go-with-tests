cf67738563b116ab0df9c2b50941ff0d7216d67e
Felipe Araújo <97049251+felipelaraujo@users.noreply.github.com>
2023-11-21 13:21:28 -0300

    add mocking

---
+++ 009-mocking/countdown.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
+
+func Countdown(writer io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(writer, i)
+		sleeper.Sleep()
+	}
+
+	fmt.Fprint(writer, finalWord)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
